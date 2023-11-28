27726f5a55e7032579734ee2df8c3317f5e3033f
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-11-28 16:33:08 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,11 +1,3 @@
-/**
-* @description The function `add` takes two arguments `x` and `y`, and returns their
-* sum.
-* 
-* @param { number } x - In the function `add=(x,%20y)=>x+y`, the `x` input parameter
-* is an operator that performs addition on the result of `x` and `y`.
-* 
-* @param { number } y - In the provided arrow function `add=(x++,y++){return x+y}`,
-* there is no `y` input parameter.
-*/
 const add = (x, y) => x + y;
+
+const sub = (x, y) => x - y;
