ae0370282f04ce612fda650578424050961acbab
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-15 23:41:32 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,19 +1,6 @@
 
 
 def hello(name: str) -> str:
-
-  """
-  This function takes a string argument `name` and returns a string with the
-  greeting "hello" followed by a comma and the input `name`.
-
-  Args:
-      name (str): The `name` input parameter is passed to the string format
-          specification inside the function's body.
-
-  Returns:
-      str: The output returned by this function is "hello.
-
-  """
   return f"hello, {name}!"
 
 if __name__ == "__main__":
