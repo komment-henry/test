708652b8358d13e29452c93e4bfcdc79ddc5a7ab
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-15 23:37:47 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,20 +2,6 @@
 
 def hello(name: str) -> str:
 
-  """
-  This function takes a string argument `name` and returns a string greeting with
-  the format "hello、[name]！".
-
-  Args:
-      name (str): The `name` input parameter is passed as a string and is used
-          inside the function to personalize the greeting message by including the
-          name of the person or thing being greeted.
-
-  Returns:
-      str: The output returned by the function "hello" is "hello,...,!" (with an
-      ellipsis representing the string " ".
-
-  """
   return f"hello, {name}!"
 
 if __name__ == "__main__":
