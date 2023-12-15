70113aff70929b6f76d9b627d54d3037cce59ce0
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-15 23:33:08 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,19 +1,7 @@
 
 
 def hello(name: str) -> str:
-  """
-  This function takes a string parameter named "name" and returns a string greeting
-  with the name included (e.g.
 
-  Args:
-      name (str): The `name` input parameter is a string that is passed as an
-          argument to the `hello` function.
-
-  Returns:
-      str: The output returned by the function `hello` with an undefined `name`
-      argument is "Hello,.!"
-
-  """
   return f"hello, {name}!"
 
 if __name__ == "__main__":
