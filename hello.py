ea90758919b0ba8eca36bec17014b7428a09aa04
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-16 00:37:19 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,18 +1,6 @@
 
 
 def hello(name: str) -> str:
-  """
-  The `hello` function takes a string `name` as input and returns a string greeting
-  with the format "hello,\tonly{name}!"
-
-  Args:
-      name (str): The `name` input parameter is a string that is passed to the
-          function and is used inside the function as part of the output message.
-
-  Returns:
-      str: The output returned by the function `hello` is "hello.
-
-  """
   return f"hello, {name}!"
 
 if __name__ == "__main__":
