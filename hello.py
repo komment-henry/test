815f855a33b1f261ec0b65fd4ae234c96bda7c65
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-16 00:53:53 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,38 +1,9 @@
 
 
 def hello(name: str) -> str:
-  """
-  The function `hello` takes a string argument `name` and returns a string with
-  the message "hello" followed by a comma and the input `name`.
-
-  Args:
-      name (str): The `name` input parameter is passed as a string and is used
-          inside the formatted string literal to produce the output greeting with
-          the user's name.
-
-  Returns:
-      str: The output returned by this function is "hello World!"
-
-  """
   return f"hello, {name}!"
 
 def add(x, y):
-  """
-  This function takes two arguments `x` and `y`, adds them together and returns
-  the sum.
-
-  Args:
-      x (float): The `x` input parameter adds the value passed to it when calling
-          the function (e.g., `add(2)`, where `2` would be added to the result of
-          adding `y` to `0`) before returning the result.
-      y (int): In the function `add(x., y)`, `y` is a second input parameter that
-          is added to `x`.
-
-  Returns:
-      int: The output returned by the function `add(x=42.5+5)` would be `97.5`,
-      which is the sum of the input parameters.
-
-  """
   return x + y
 
 if __name__ == "__main__":
