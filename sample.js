058ed9c0e7a5c93d205e136b6db9078c6e4f2bc5
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-18 11:40:57 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,28 +1,8 @@
-/**
-* @description The function `add` takes two arguments `x` and `y` and returns their
-* sum.
-* 
-* @param { number } x - In the function add = (x., y.) -> x+y., the input parameter
-* x is a generic name for an unspecified value that will be added to y to produce
-* the output of the function.
-* 
-* @param { number } y - In the given function `add=(x、y)=>(x+y)`, the `y` input
-* parameter represents the second number to be added to the first number `x`.
-*/
+
 const add = (x, y) => x + y;
 
 /**
 */
 const sub = (x, y) => x - y;
 
-/**
-* @description The function `mul` takes two arguments `x` and `y`, and returns their
-* product (the result of multiplying them together).
-* 
-* @param { number } x - The `x` input parameter multiplies the value of `y`.
-* 
-* @param { number } y - In the provided JavaScript function `mul`, the `y` input
-* parameter is multiplied element-wise with the `x` input parameter to produce the
-* output.
-*/
 const mul = (x, y) => x * y;
