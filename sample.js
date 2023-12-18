b61cb002918fccfaa0b6818433e3787ec74e02d4
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-18 14:17:55 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,4 +1,3 @@
-
 /**
 * @description This function takes two arguments `x` and `y`, and returns their sum.
 * 
@@ -10,37 +9,8 @@
 */
 const add = (x, y) => x + y;
 
-/**
-* @description This function subtracts `x` from `y`.
-* 
-* @param { number } x - The `x` input parameter is the first operand of the subtraction
-* operator (-) and it represents the value that is being subtracted from `y`.
-* 
-* @param {  } y - In the provided Function expression `(x) => x - void 0`, the `void
-* 0` represents the `y` parameter. It doesn't do anything.
-*/
 const sub = (x, y) => x - y;
 
-/**
-* @description The function `mul` takes two arguments `x` and `y`, and returns their
-* product (`x * y`).
-* 
-* @param { number } x - In the given function `mul`, the `x` input parameter is
-* multiplied by the `y` input parameter to produce the output of the function.
-* 
-* @param { number } y - The `y` input parameter multiplies the result of the function.
-*/
 const mul = (x, y) => x * y;
 
-/**
-* @description This function takes two arguments `x` and `y`, and returns the value
-* of `x` divided by `y`, but only if `y` is non-zero.
-* 
-* @param { number } x - The `x` input parameter is multiplied by `y`, so it has no
-* effect if `y` is equal to 0.
-* 
-* @param { number } y - In the function `(x，y) → y === 0 ? 0 : x / y`, the `y` input
-* parameter determines whether the function returns zero or the result of dividing
-* `x` by `y`.
-*/
 const div = (x, y) => y === 0 ? 0 : x / y;
