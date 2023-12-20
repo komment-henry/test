4bfe1b01a7f0984807b626169db71763561f9d3a
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-20 17:51:30 +0000

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,15 +1,1 @@
 
-/**
-* @description This function does nothing since it is not defined.
-*/
-function abc() => {
-/**
-* @description The function throws a `NullReferenceException`.
-*/
-  return { _do: () => { throw null; } }
-}
-
-
-
-
-
