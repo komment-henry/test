79b9a6c55014d9703701fe947688433d85dce731
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-20 15:16:03 +0000

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,16 +1,4 @@
 
-
-/**
-* @description The function takes two arguments `y` and `z`, and returns the difference
-* between them: `y - z`.
-* 
-* @param { integer } y - In the function `x = (y: z) => y - z`, the `y` input parameter
-* is not used at all.
-* 
-* @param { number } z - The `z` input parameter is not used at all inside the function
-* body of `x`. It is merely a parameter that is passed into the function and is not
-* read or referred to within the function.
-*/
 const x = (y, z) => y - z;
 
 
