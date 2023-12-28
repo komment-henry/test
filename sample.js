63fea3624de2358910138893ffcea7ab7d5c92b8
komment-henry <151629959+komment-henry@users.noreply.github.com>
2023-12-28 21:10:32 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,50 +1,10 @@
-/**
-* @description This function takes two arguments `x` and `y`, and returns their sum.
-* 
-* @param { number } x - In the function `add = (x)`. the `x` input parameter is the
-* first argument passed to the function when it is called.
-* 
-* @param { number } y - In the function `add=(x、y)=>x+y`, the `y` input parameter
-* is added to the `x` input parameter to produce the sum.
-*/
+
 const add = (x, y) => x + y;
 
-/**
-* @description The given function `sub` takes two arguments `x` and `y`, and returns
-* the difference of `x` and `y`.
-* 
-* @param { number } x - The `x` input parameter is the first operand of the subtraction
-* operator and represents the value that will be subtracted from `y`.
-* 
-* @param { number } y - The `y` input parameter subtracts its value from the `x`
-* input parameter within the function's return statement. In other words.
-*/
 const sub = (x, y) => x - y;
 
-/**
-* @description The function `mul` takes two arguments `x` and `y` and returns their
-* product `x * y`.
-* 
-* @param { number } x - The `x` input parameter is multiplied by the `y` input
-* parameter within the function.
-* 
-* @param { number } y - In the function `mul=(x.y)`, `y` is a parameter that receives
-* a value when the function is called. It serves as the second operand for multiplication
-* and plays no role elsewhere inside the function body.
-*/
 const mul = (x, y) => x * y;
 
-/**
-* @description This function takes two arguments `x` and `y`, and returns the result
-* of dividing `x` by `y`, unless `y` is zero then it returns 0.
-* 
-* @param { number } x - The `x` input parameter divides the result of the division
-* by the divisor when the `y` input parameter is not equal to zero.
-* 
-* @param { number } y - The `y` input parameter is used to determine the divisor for
-* the `x` parameter. If `y` is equal to zero (using the === operator), then the
-* function returns 0.
-*/
 const div = (x, y) => y === 0 ? 0 : x / y;
 
 
